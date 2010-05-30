4d7367521f900ebee3637a2e8e6ba8e0ca4bef65
Dmitry Neverov <dmitry.neverov@gmail.com>
2010-05-30 20:47:06 +0400

    ex. 3.2.4

---
+++ ch03/3.2.4.hs
@@ -0,0 +1,10 @@
+(m + n) + p = m + (n + p)
+p = Zero:
+left part: (m + n) + Zero = (m + n) = m + n
+right part: m + (n + Zero) = m + n
+
+let's suppose (m + n) + p = m + (n + p), for all p <= x
+p = Succ x
+left part: (m + n) + Succ x = Succ ((m + n) + x)
+right part: m + (n + Succ x) = m + Succ (n + x) = Succ (m + (n + x))
+
