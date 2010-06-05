ae645aec93bf9b95bce1959007eb4a327de38926
Dmitry Neverov <dmitry.neverov@gmail.com>
2010-06-05 22:03:31 +0400

    ex. 3.5.1

---
+++ ch03/3.5.1.hs
@@ -0,0 +1,4 @@
+let's suppose there is (u, v) which represents same rational and v > 0, gcd(u, v) = 1, u /= x, v /= y
+x / y = u / v => 
+1. x = (u * y) / v, since gcd(u, v) = 1, v should devide y
+2. u = (x * v) / y, since gcd(x, y) = 1, y should devide v => v == y and x == u => there is no u /= x, v /= y representing same rational nubmer
