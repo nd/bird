527b2e9d63599073836138301662862f255655f5
Dmitry Neverov <dmitry.neverov@gmail.com>
2010-06-05 21:26:30 +0400

    ex 3.4.3

---
+++ ch03/3.4.3.hs
@@ -0,0 +1,5 @@
+-- (floor (floor x)) == (floor x)
+--  
+-- it is the case if x is integer, because for integer (floor x) == x
+--  
+-- but sinse (floor x) gives us integer (floor (floor x)) == (floor x) also for all real numbers
