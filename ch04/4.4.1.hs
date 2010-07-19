eebd925304504413fc8da8520ae475550c5bfd05
Dmitry Neverov <dmitry.neverov@gmail.com>
2010-07-19 07:59:25 +0400

    ex. 4.4.1

---
+++ ch04/4.4.1.hs
@@ -0,0 +1,7 @@
+unzip [undefined] = undefined
+
+zipp (unzip [(undefined, undefined)]) = 
+{def of unzip}
+zipp ([undefined], [undefined]) =
+{case exhaustion in zipp} =
+    undefined which is not equal to [(undefined, undefined)]
