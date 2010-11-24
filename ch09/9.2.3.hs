59d110938a1faa4b8cffdffba080e5f83f386e37
Dmitry Neverov <dmitry.neverov@gmail.com>
2010-11-24 23:35:51 +0300

    ex. 9.2.3

---
+++ ch09/9.2.3.hs
@@ -0,0 +1,5 @@
+suppose such function exists, then 
+sort 3:2:1:undefined <= (appr order) sort 3:2:1:0:undefined (since 3:2:1:undefined <= (approximation order)  3:2:1:0:undefined)
+but sort should return 
+1:2:3:undefined in first case, which is not <= (approximation order) 0:1:2:3:undefined
+therefor it is imposible
