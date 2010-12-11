33490558fcf238fac316540e571f5e27d6582084
Dmitry Neverov <dmitry.neverov@gmail.com>
2010-12-11 23:34:14 +0300

    ex. 10.2.1

---
+++ ch10/10.2.1.hs
@@ -0,0 +1,35 @@
+type Exception = String
+data Exc a = Raise Exception | Return a
+
+instance Monad Exc where
+    return x = Return x
+    (Raise e)  >>= q = Raise e
+    (Return x) >>= q = q x
+
+
+raise :: Exception -> Exc a
+raise e = Raise e
+
+data Term = Con Int | Div Term Term | Try Term Term
+
+instance Show a => Show (Exc a) where
+    show (Raise e)  = "exception: " ++ e
+    show (Return x) = "value: " ++ show x
+
+evalEx :: Term -> Exc Int
+evalEx (Con x)   = return x
+evalEx (Div t u) = do x <- evalEx t
+                      y <- evalEx u
+                      if y == 0 
+                         then raise "division by zero"
+                         else return (x `div` y)
+evalEx (Try t u) = do f(evalEx t)
+                      where 
+                        f (Return x) = Return x
+                        f (Raise e) = do evalEx u
+                                         
+
+
+answer, wrong :: Term
+answer = Div (Div (Con 1972) (Con 2)) (Con 23)
+wrong  = Div (Con 2) (Div (Con 1) (Con 0))
